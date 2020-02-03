d203f790af301e7e2d39405419f31ca1264a415f
Vitaly Oryschuk <Leilyua@ukr.net>
2020-02-03 11:56:05 +0200

    Updates

---
+++ precache-manifest.a7d8d12bba8b9599d3f42cb6275d54c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbf8ed6842d942ebe34cc8b1946c94e",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "30426c1b1a73d8292c2b",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5d7a150d6f1c9ff6229e",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.72777275.chunk.css"
+  },
+  {
+    "revision": "30426c1b1a73d8292c2b",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.9bf78f80.chunk.js"
+  },
+  {
+    "revision": "5d7a150d6f1c9ff6229e",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.28b9a226.chunk.js"
+  },
+  {
+    "revision": "22a2095e95bb4596531e",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.0b8c1f63.js"
+  }
+]);
